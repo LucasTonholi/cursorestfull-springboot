6e5c915816574a507c07ff44bae6241e719c043f
Lucas <lucasfire00@gmail.com>
2020-08-24 20:17:39 -0300

    meu primeiro commit

---
+++ src/main/java/com/example/cursorstrufulspringboot/CursorstrufulSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursorstrufulspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorstrufulSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorstrufulSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cursorstrufulspringboot/CursorstrufulSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursorstrufulspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorstrufulSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
